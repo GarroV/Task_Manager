9bd0ea116ec8436199a692382a2fb0fc4f701941
GarroV <62515132+GarroV@users.noreply.github.com>
2023-01-07 15:41:48 +0100

    Update test.java

--- src/models/test.java
+++ src/models/test.java
@@ -9,6 +9,17 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class test {
-    //public static void main(String[] args) {   }
+    /**public static void main(String[] args) {
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println(" Вы уверены? y/n");
+        String answer = scanner2.nextLine();
+
+        System.out.println("Выбран вариант " + answer);
+        String ra = "y";
+        if (answer.equals(ra)) {
+            System.out.println("Заработало");
+        } else System.out.println("Завершение отменено");   }**/
 }
